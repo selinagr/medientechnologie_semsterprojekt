6cc9c7f63a28e0bd9b8715f7fbb8b0985fafd5d9
selinagr <selina-sally94web.de>
2015-01-13 18:05:10 +0100

    funktionierende Klicks

---
+++ scripts.js
@@ -0,0 +1,18 @@
+var selina = $("#testD");
+
+selina.click(function(event){
+	event.preventDefault();
+	//console.log("hello");
+	$("#disziplinen").toggleClass("disziplinen");
+});
+
+var zwei = $("#testA");
+
+zwei.click(function(event){
+	event.preventDefault();
+	//console.log("hello");
+	$("#udrudern").toggleClass("udrudern");
+});
+
+
+
